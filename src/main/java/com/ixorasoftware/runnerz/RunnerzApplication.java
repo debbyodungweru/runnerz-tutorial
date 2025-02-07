7ff036f52ee3bc6a05d57c178941521749891852
Deborah Odungweru <deborah.odungweru@gmail.com>
2025-02-07 21:51:15 +0000

    fix: Minor fixes to the RestClient code

--- src/main/java/com/ixorasoftware/runnerz/RunnerzApplication.java
+++ src/main/java/com/ixorasoftware/runnerz/RunnerzApplication.java
@@ -1,18 +1,16 @@
 package com.ixorasoftware.runnerz;
 
-import com.ixorasoftware.runnerz.run.Run;
-import com.ixorasoftware.runnerz.user.User;
-import com.ixorasoftware.runnerz.user.UserHttpClient;
 import com.ixorasoftware.runnerz.user.UserRestClient;
+import com.ixorasoftware.runnerz.user.User;
+import com.ixorasoftware.runnerz.run.Run;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.client.support.RestClientAdapter;
-import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.util.List;
+
+@SpringBootApplication
 public class RunnerzApplication {
 
 	public static void main(String[] args) {
@@ -20,31 +18,19 @@ public class RunnerzApplication {
 	}
 
 	@Bean
-	UserHttpClient userHttpClient()
-	{
-		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
-		HttpServiceProxyFactory factory = HttpServiceProxyFactory
-				.builderFor(RestClientAdapter.create(restClient))
-				.build();
-		return factory.createClient(UserHttpClient.class);
-	}
-
-	@Bean
-	CommandLineRunner runner(UserRestClient client, UserHttpClient userHttpClient)
+	CommandLineRunner runner(UserRestClient client)
 	{
 		return args -> {
-//			List<User> users = c
-//
-//          client.findAll();
-//			System.out.println(users);
-//
-//			User user = client.findById(1);
-//			System.out.println(user);
+			List<User> users = client.findAll();
 
-//			Run run = client.findRun(1);
-//			System.out.println(run);
+            client.findAll();
+			System.out.println(users);
 
-			System.out.println(userHttpClient.findById(1));
+			User user = client.findById(1);
+			System.out.println(user);
+
+			Run run = client.findRun(1);
+			System.out.println(run);
 		};
 	}
 
